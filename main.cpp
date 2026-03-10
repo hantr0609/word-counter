2755722bbf7d0f0ea1057e191fdd07de9d4c6f2a
hantr0609 <hantr060902@gmail.com>
2026-03-09 23:26:24 -0500

    Add a simple word counter

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <cstdint>
+#include <cctype>
 
 int main() {
-  std::vector<std::string> msg{"A", "simple", "word", "counter", "with", "C++"};
+  std::ifstream data("words.txt");
+  std::string content((std::istreambuf_iterator<char>(data)), std::istreambuf_iterator<char>());
+  uint32_t wordCount = 0;
 
-  for (const std::string& word : msg) {
-    std::cout << word << " ";
-
+  for (char& ch : content) {
+    if (std::isspace(ch)) {
+      wordCount++;
+    }
   }
 
-  std::cout << std::endl;
+  // To return actually total words by increase the counter one more
+  // But this will cause an issue if the input file is empty
+  wordCount++;
+
+  std::cout << "Word: " << wordCount << " ";
 }
